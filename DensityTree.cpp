6f2b53bba4272bb245b8ecce81e407669658f0d1
kksgit <wangkang4518@gmail.com>
2017-07-07 21:40:11 +0200

    update
    添加了对于theta和infogain的遍历求解，下一步需要算出LS和RS的高斯分布，然后存储高斯分布的mean和sd。
    然后再densityXY中通过上面算出来的高斯分布，遍历所有数据，到得1000个data的density，得到的density也是一个1000×2的矩阵。

--- DensityTree.cpp
+++ DensityTree.cpp
@@ -1,18 +1,57 @@
 #include "DensityTree.h"
 #include <iostream>
+#include <random>
+#include <cmath>
 using namespace cv;
 using namespace std;
+
+double getinfoGain()(Mat& LS, Mat& RS, Mat& S)
+
+
 DensityTree::DensityTree(unsigned int D, unsigned int n_thresholds, Mat X) 
 {
     this-> D=D;
     this-> X=X;
     this-> n_thresholds=n_thresholds;
 }
+
+
+// generate random value from 3 to 7 ===> rand()/(float)(RAND_MAX)*4+3
 void DensityTree::train()
 {
-	double minValX, maxValX;
+	double minVal, maxVal,theta;
+	/* iterate twice for X and Y */
 	for (int i = 0; i < 2; i++) {
-		minMaxIdx(X.col(i), &minValX, &maxValX, NULL, NULL);
+		minMaxIdx(X.col(i), &minVal, &maxVal);// get the max and min value in col(0,1)
+		
+		/*----- iterate n times to get n thetas, choose the largest infoGain---*/
+		Mat LS, RS, LS_final, RS_final;
+		double max_infoGain = 0, theta_split;
+		for (int j = 0; j < n_thersholds; j++){
+			theta=rand()/(double)(RAND_MAX)*(maxVal-minVal)+minVal;  // get the splitpoint
+			LS.release();
+			RS.release();
+			
+			//sperate X into LS and RS
+			for(int p = 0; p < X.rows ; p++ ){
+				double temp = x.at<double>(p,i)
+				if(temp <= theta )
+				LS.push_back(temp);
+				else RS.push_back(temp);
+			   	}                
+			double  infoGain = getinfoGain(LS, RS, X);
+			if (infoGain > max_infoGain) 
+					max_infoGain = infoGain;
+					theta_split = theta;
+					LS_final = LS.clone();		
+					RS_final = RS.clone();
+			 }
+			 //meanStdDev(M,temp_m,temp_sd)
+			 
+		
+		
+		
+		}
 
 	int N = pow(2, D - 1);//no. of leaf nodes
 
@@ -26,16 +65,16 @@ void DensityTree::train()
 								  //}
 
 								  //max min
-			double minv = 0.0, maxv = 0.0, range = 0.0;
-			double* minp = &minv;
-			double* maxp = &maxv;
-			Mat	X1 = X.col(0).clone();//X.colRange(0, 1).clone();
-			minMaxIdx(X1, minp, maxp);
-			range = maxv - minv; //around 32.9369
+			//double minv = 0.0, maxv = 0.0, range = 0.0;
+			//double* minp = &minv;
+			//double* maxp = &maxv;
+			//Mat	X1 = X.col(0).clone();//X.colRange(0, 1).clone();
+			//minMaxIdx(X1, minp, maxp);
+			//range = maxv - minv; //around 32.9369
 								 //cout << minv << endl;
 
-			Mat leftSet, rightSet;// , leftResult, rightResult;//left set; right set
-			double splitPoint, InfoGain = 0.0;
+			//Mat leftSet, rightSet;// , leftResult, rightResult;//left set; right set
+			//double splitPoint, InfoGain = 0.0;
 			for (int i = 0; i < n_thresholds; i++)
 			{
 				InfoGain = 0.0;
@@ -89,7 +128,7 @@ void DensityTree::train()
 			variances.push_back(getVariance(rightResult, mean));
 
 			cout << "Not implemented " << means.size() << endl;//Temporla
-		}
+		
 	
 	
 	
@@ -106,5 +145,23 @@ Mat DensityTree::densityXY()
     return X;//Temporal
 }
 
+
+/*------get infoGain------*/
+double getinfoGain()(Mat& LS, Mat& RS, Mat& S) {
+	
+	Mat mean,LCovar, RCovar, SCovar;
+	
+	// calculate the covariance matrix
+	calcCovarMatrix(LS, LCovar, mean, CV_COVAR_NORMAL|CV_COVAR_ROWS);  //we only want covariance.
+	calcCovarMatrix(RS, RCovar, mean, CV_COVAR_NORMAL|CV_COVAR_ROWS);
+	calcCovarMatrix(S, SCovar, mean, CV_COVAR_NORMAL|CV_COVAR_ROWS);
+		
+	// calculate the determinant and infoGain
+	double infoGain = log(determinant(SCovar)) - (double)LS.rows / (double)S.rows*log(determinant(LCovar)) - (double)RS.rows / (double)S.rows*log(determinant(RCovar));
+	
+	
+	return infoGain;
+
+}
 
 
